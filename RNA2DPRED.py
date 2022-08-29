47eae100db18963c94a9e2c154ac2a234ec9e877
Amata Dhātu <70560694+LeKhang97@users.noreply.github.com>
2022-08-29 11:43:24 +0200

    Add files via upload

--- RNA2DPRED.py
+++ RNA2DPRED.py
@@ -56,8 +56,12 @@ def valid_motif(struct, pseudoknot = False):
     if struct[0] == '&' or struct[len(struct)-1] == '&':
         return False
 
-    if len(struct.replace('&','').replace('_','')) <= 10:
+    if len(struct.replace('&','').replace('_','')) <= 3:
         return False
+
+    if '&' in struct or '_' in struct:
+        if len(struct.replace('&','').replace('_',''))/(struct.count('&') + struct.count('_')) < 3:
+            return False
 
     if any(struct.count(i) != struct.count(j) for i,j in zip(['(', '[', '<'], [')', ']', '>'])) or (struct.count('(') + struct.count('[') + struct.count('<') == 1):
         return False
@@ -111,8 +115,8 @@ def main_argument():
     parser.add_argument('-t',
         '--threshold',
 		type = float,
-		default = 0.01,
-        help ='Pairing probability threshold. Default = 0.01.')
+		default = 0.00001,
+        help ='Pairing probability threshold. Default = 0.00001.')
 
     parser.add_argument('-s',
         '--sequence',
@@ -218,13 +222,34 @@ if __name__ == "__main__":
         prob = RNA.get_pr(i, j) 
         if prob != 0:
             if -math.log10(prob) < -math.log10(args.threshold):
-                pairing_dict[(j-1,i-1)] = round(prob,5)
-                
+                pairing_dict[(j-1,i-1)] = round(prob,int(-math.log10(args.threshold)) + 1)
+
+    if args.verbose:
+        print("Creating forbidden combination of motifs...")
+    list_not_combine = []
+    for motif1, motif2 in itertools.combinations(filtered_motif_dict,2):
+        for i in flatten(filtered_motif_dict[motif1]['pos']):
+            if i in flatten(filtered_motif_dict[motif2]['pos']):
+                if args.verbose:
+                    print("adding {} to list...".format((motif1, motif2)))
+                list_not_combine += [(motif1, motif2)]
+                break
+        if pkn == False:
+            if not((motif1, motif2) in list_not_combine):
+                for i,j in itertools.product(filtered_motif_dict[motif1]['pairing_pos'], 
+                filtered_motif_dict[motif2]['pairing_pos']):
+                    if ((j[1] >= i[1] and j[1] <= i[0] and j[0] >= i[0]) or 
+                    (i[1] >= j[1] and i[1] <= j[0] and i[0] >= j[0])):
+                        if args.verbose:
+                            print("adding {} to list...".format((motif1, motif2)))
+                        list_not_combine += [(motif1, motif2)]
+                        break
+
     # Integer Programming
     num = 1
     superpose = True
     if args.verbose:
-        print("Begin to solve integer programming problem...")
+        print("\nBegin to solve integer programming problem...\n")
 
     sol_val_list = []
     sol_var_list = []
@@ -233,23 +258,23 @@ if __name__ == "__main__":
         x = m.binary_var_dict(filtered_motif_dict, name = '')
         y = m.binary_var_dict(pairing_dict, name = '')
 
-        if args.verbose:
-            print("Adding constraints to avoid overlapping motifs...")
-
         # Avoid overlapping motifs
-        m.add_constraints(x[i]*len(flatten(filtered_motif_dict[i]['pos'])) + x[j]*len(flatten(filtered_motif_dict[j]['pos'])) 
-        <= len(set(flatten(filtered_motif_dict[i]['pos']) + flatten(filtered_motif_dict[j]['pos'])))
-        for i,j in itertools.combinations(filtered_motif_dict,2)) 
+        if args.verbose:
+            if pkn == True:
+                print("Adding constraints to avoid overlapping motifs...")
+            else:
+                print("Adding constraints to avoid overlapping motifs and pseudoknots from motifs...")
+        for i in list_not_combine:
+            m.add_constraint(x[i[0]] + x[i[1]] <= 1)
 
         if args.verbose:
             print("Adding constraints to unexpected pairings in motif regions...")
         # Avoid probabilistic basepairs in motif region:
-        for motif in filtered_motif_dict:
-            for comp in filtered_motif_dict[motif]['pos']:
-                for pair in pairing_dict:
-                    if ((pair[0] <= comp[0] and pair[0] >= comp[len(comp)-1]) or 
-                    (pair[1] <= comp[0] and pair[1] >= comp[len(comp)-1])):
-                        m.add_constraint(x[motif] + y[pair] <= 1)
+        for motif in filtered_motif_dict:   
+            for pair in pairing_dict:
+                if ((pair[0] in flatten(filtered_motif_dict[motif]['pos'])) or
+                (pair[1] in flatten(filtered_motif_dict[motif]['pos']))):
+                    m.add_constraint(x[motif] + y[pair] <= 1)
 
         # Avoid pseudoknot (if selected)
         if pkn == False:
@@ -270,16 +295,6 @@ if __name__ == "__main__":
                     or (i[1] >= j[1] and i[1] <= j[0] and i[0] >= j[0])):
                         m.add_constraint(y[i] + x[motif] <= 1)
 
-            if args.verbose:
-                print("Adding constraints to avoid pseudoknots from motifs...")
-            # Avoid pseudoknot from motifs
-            for motif1, motif2 in itertools.combinations(filtered_motif_dict,2):
-                for i,j in itertools.product(filtered_motif_dict[motif1]['pairing_pos'], filtered_motif_dict[motif2]['pairing_pos']):
-                    if not(i in pairing_dict) or not(j in pairing_dict):
-                        if ((j[1] >= i[1] and j[1] <= i[0] and j[0] >= i[0]) 
-                        or (i[1] >= j[1] and i[1] <= j[0] and i[0] >= j[0])):
-                            m.add_constraint(x[motif1] + x[motif2] <= 1)
-
         # Avoid lone basepair (under development)
 
         # Objective function
@@ -288,6 +303,12 @@ if __name__ == "__main__":
             for pair in filtered_motif_dict[motif]['pairing_pos']:
                 if pair in pairing_dict:
                     prob_from_motifs += x[motif]*pairing_dict[pair]
+
+        if num == 1:
+            n = m
+            n.set_objective('max',sum([x[i]*sum([len(comp)**2 
+            for comp in filtered_motif_dict[i]['pos']]) for i in filtered_motif_dict]))
+            t = n.solve()
 
         # Function A and MEA (Prioritize MEA) (the greater number, the more priority)
         m.set_multi_objective('max', [sum([x[j]*sum([len(comp)**2 for comp in filtered_motif_dict[j]['pos']]) 
@@ -306,10 +327,15 @@ if __name__ == "__main__":
                 sum(y[i] for i in pairing_dict if not(i in pre_sol)) >= 1 
                 for pre_sol in sol_var_list)
 
-                m.add_constraint(sum([x[i]*sum([len(comp)**2 for comp in filtered_motif_dict[i]['pos']]) for i in filtered_motif_dict]) >= sol_val_list[-1][0])
+                m.add_constraint(sum([x[i]*sum(len(comp)**2 for comp in filtered_motif_dict[i]['pos']) for i in filtered_motif_dict]) >= sol_val_list[-1][0])
             else:
-                m.add_constraint(sum([x[i]*sum([len(comp)**2 for comp in filtered_motif_dict[i]['pos']]) for i in filtered_motif_dict]) >= sol_val_list[-1][0] + 0.05)
-            
+                #if  sol_val_list[-1][0] <= t.objective_value - args.threshold:
+                if  sol_val_list[-1][0] <= t.objective_value:
+                    m.add_constraint(sum([x[i]*sum(len(comp)**2 for comp in filtered_motif_dict[i]['pos']) for i in filtered_motif_dict]) >= sol_val_list[-1][0] + args.threshold)
+                else:
+                    print("\nNo more non-dorminated solutions!")
+                    break
+
             #m.add_constraint(sum([y[i]*pairing_dict[i] for i in pairing_dict]) + prob_from_motifs >= sol_val_list[-1][1] + 0.005)
 
         sol = m.solve()
@@ -344,9 +370,47 @@ if __name__ == "__main__":
 
         num += 1
 
-    print("\nThe whole Pareto set is: {0} ".format(sol_val_list))
+    # Show the result
+    sol_pair_list = []
+    out_align = ''
+    sol_dict = {}
+    s = 1
+    for sol, score in zip(sol_var_list,sol_val_list):
+        x = []
+        align = ''
+        for i in sol:
+            if type(i) == tuple:
+                x += [i] 
+            else:
+                x += filtered_motif_dict[i]['pairing_pos']
+                align_site = ' '*len(seq)
+                for u in flatten(filtered_motif_dict[i]['pos']):
+                    align_site = align_site[:u] + '-' + align_site[(u + 1):]
 
-    print("\nThe whole variable set is: {0} ".format(sol_var_list))
+                align += align_site + '\t' + i.split('_')[0] + '\n'
+        struct2d = '.'*len(seq)
+        for i in x:
+            struct2d = struct2d[:i[1]] + '(' + struct2d[(i[1] + 1):i[0]] + ')' + struct2d[(i[0] + 1):]
+        
+        print(struct2d + '\t' + str(score[0]) + '\t' + str(score[1]))
+        print(align + '\n')
+        out_align += f'Solution_{s}:\n' + struct2d + '\t' + str(score[0]) + '\t' + str(score[1]) + '\n' + align + '\n'
+        sol_pair_list += [x]
+        sol_dict[f'Solution_{s}'] = {'Score': score,
+                                    'Motif':[i.split('_')[0] for i in sol_var_list[s-1] if type(i) == str],
+                                    'Base_pair': x}
+        s += 1
 
+    #print("\nThe whole Pareto set is: {0} \n".format(sol_val_list))
+    #print("The whole variable set is: {0} ".format(sol_var_list))
+    print("The result dict is: {0} ".format(sol_dict))
+    #print("\nThe whole pair set is: {0} \n".format(sol_pair_list))
 
+    if args.outfile:
+        with open('aligned_' + args.outfile,'w') as outfile1:
+                outfile1.write(out_align)
 
+        with open(args.outfile,'w') as outfile2:
+                outfile2.write(json.dumps(sol_dict, indent= 2))
+    
+    
